50df14229495140661cae74004b4b6f1267cec6d
덩얼 <seoyeom542@deong-eol-ui-MacBookAir.local>
2025-10-08 12:19:10 +0900

    문자열 데이터 타입 학습

---
+++ 생활코딩/4_2.py
@@ -0,0 +1,19 @@
+print('hello world')
+print("hello world")
+
+# 여러줄
+print('''
+hello
+word
+''')
+print("""
+hello
+word
+""")
+
+print("'1'+'1': ", '1'+'1') # 산술 연산 x, 결합 연산 o
+print('hello word'*2)
+
+print(len('hello word'*2))
+
+print('replace: ', 'hello world'.replace('world', 'home'))

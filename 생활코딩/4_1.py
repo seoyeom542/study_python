248f244525fb1456b9477ba40c39f2c4e392b0ed
덩얼 <seoyeom542@deong-eol-ui-MacBookAir.local>
2025-10-08 12:03:14 +0900

    숫자형 데이터 타입 학습

---
+++ 생활코딩/4_1.py
@@ -0,0 +1,21 @@
+print(-1) # 음의 정수
+print(0)
+print(1) # 양의 정수 / int
+
+print(1.1) # 실수 / float
+
+# 사칙연산
+print(1+1)
+print('1+1: ', 1+1)
+print('2-1:', 2-1)
+print('2*1: ', 2*1)
+print('4/2: ', 4/2)
+
+import math
+
+print('sqrt: ', math.sqrt(4)) # 제곱근
+print('pow: ', math.pow(4, 2)) # 제곱 a를 b번 곱한 값 출력
+
+import random
+
+print('random: ', random.random())

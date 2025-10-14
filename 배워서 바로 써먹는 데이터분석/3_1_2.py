e5a503c5f986e283546e5a3a33e702ea27293df9
덩얼 <seoyeom542@deong-eol-ui-MacBookAir.local>
2025-10-15 08:11:59 +0900

    책 3.1.2 예제 실습

---
+++ 배워서 바로 써먹는 데이터분석/3_1_2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print(np.zeros(3)) # 0이 3개 들어있는 배열 생성
+
+print('\n# zeros((2,2)) : 요소의 값이 모두 0인 2 * 2의 배열 생성')
+arrZeros = np.zeros((2,2)) # 2차원 배열 생성  
+print(arrZeros)
+
+print('\n# ones((3,2)) 배열 생성')
+array2 = np.ones((3,2)) # 3열, 2행
+print(array2)
